52ab7c1370319cf0235a932503851e89696e4e72
unknown <rajveerrayat54@gmail.com>
2025-06-12 00:39:05 +0530

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Button from "./components/Button/Button";
+//import Card from "./components/Card/Card";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
         <Navbar />
+        //<Card/>
       </div>
     </>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,9 +3,14 @@ import logo from "../../assets/images/one.jpg";
 
 const Card = ({ title, content, image }) => {
   return (<div className="card">
-    <img src="{one}">
-  </div>
-  )
-}
+    {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
+    <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+      </div>
+    </div>
+  );
+};
+
 
 export default Card;
