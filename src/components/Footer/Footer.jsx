322f5e4d7275ca7d61e8e09a65008ce38b3e924c
unknown <rajveerrayat54@gmail.com>
2025-06-17 22:43:43 +0530

    your msg

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,39 @@
-// Footer.jsx
 import React from "react";
-import logo from "../../assets/images/logo.png";
 import "./Footer.css";
+import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="flogo">
-        <h3>HuggebyRoop</h3>
-        <img src={logo} alt="Logo" />
+      <div className="footer-section">
+        <h3>HugbyRoop</h3>
+        <img src={logo} />
+        <p>© 2025 MyWebsite. All rights reserved.</p>
       </div>
-      <div className="home">
-        <h4>Home</h4>
+      <div className="footer-section">
+        <h4>Quick Links</h4>
         <ul>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <a href="#">Privacy Policy</a>
-          </li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Follow Us</h4>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Twitter</a></li>
+        </ul>
+      </div>
+
+      <div className="footer-section">
+        <h4>Contact Us</h4>
+        <ul>
+          <li><a href="#">www.roopcreations28.com</a></li>
+          <li><a href="#">For more information Call us : <br/>
+            +91 98542685</a></li>
+          <li><a href="#">Adress : V.P.O Kotla Nihang Rupnagar Punjab,140001 </a></li>
         </ul>
       </div>
     </footer>
