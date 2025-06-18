5548149faeb1472ef44f7430497972b30939e45c
Rajveer510 <rajveerrayat54@gmail.com>
2025-06-18 18:05:43 +0530

    pages created

---
+++ src/Page/About/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about-section">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our website! We are passionate about creating clean and
+        modern web solutions.
+      </p>
+      <p>
+        Our mission is to deliver high-quality, user-friendly digital
+        experiences for everyone.
+      </p>
+    </div>
+  );
+};
+
+export default About;
