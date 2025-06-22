db5870f5a85442a05f0beb05d75b1f2049cd82df
unknown <rajveerrayat54@gmail.com>
2025-06-22 18:48:36 +0530

    ready to pull

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import Registration from "./Page/Registration/Registration";
 import Home from "./Page/Home/Home";
 import About from "./Page/About/About";
 import Detail from "./Page/Details/Details";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import { Connect } from "./Page/Contact/Contact";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Registration" element={<Registration />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Details" element={<Detail />} />
-        <Route path="/Contact" element={<Connect />} />
-      </Routes>
-    </Router>
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Detail/>
+      <Connect/>
+      <Footer/>
+
+    </div>
   );
 };
 
